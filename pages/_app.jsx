ea7a11f64db2a1f152042b1b2881dc165f058422
Garrett John Law <85005808+GarrettJohnLaw@users.noreply.github.com>
2022-09-14 22:00:38 -0700

    updated script

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,19 @@
 import { SessionProvider } from "next-auth/react";
 import Layout from "../components/layout/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { SSRProvider } from "react-bootstrap";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <SSRProvider>
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </SSRProvider>
   );
 }
